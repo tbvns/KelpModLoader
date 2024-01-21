e3be418d435f2a343300335306024f3ad1b19c7b
tbvns <69420062+tbvns25565@users.noreply.github.com>
2024-01-21 20:21:05 +0000

    Fixed wrong "-1" placement in ItemUtils.java

--- src/main/java/xyz/prismenetwork/kelpmodloader/Item/ItemUtils.java
+++ src/main/java/xyz/prismenetwork/kelpmodloader/Item/ItemUtils.java
@@ -25,11 +25,11 @@ public class ItemUtils {
     public static ModdedItem getModedItem(ItemStack itemStack) {
         if (itemStack.hasItemMeta()) {
             ItemMeta itemMeta = itemStack.getItemMeta();
-            if (itemMeta.getCustomModelData() - 1 != 0) {
+            if (itemMeta.getCustomModelData() != 0) {
                 for (int i = 0; i < Constant.Items.size(); i++) {
                     ModdedItem item = Constant.Items.get(i);
-                    System.out.println(Constant.Items.get(i));
-                    if (itemMeta.getCustomModelData() == item.getId() - 1 && itemStack.getType() == item.ItemMaterial) {
+                    System.out.println(Constant.Items.get(i).getId());
+                    if (itemMeta.getCustomModelData() - 1 == item.getId() && itemStack.getType() == item.ItemMaterial) {
                         return item;
                     }
                 }
@@ -45,11 +45,11 @@ public class ItemUtils {
     public static ModdedBlock getModedBlockFromItem(ItemStack itemStack) {
         if (itemStack.hasItemMeta()) {
             ItemMeta itemMeta = itemStack.getItemMeta();
-            if (itemMeta.getCustomModelData() - 1 != 0) {
+            if (itemMeta.getCustomModelData() != 0) {
                 for (int i = 0; i < Constant.Blocks.size(); i++) {
                     ModdedBlock moddedBlock = Constant.Blocks.get(i);
-                    System.out.println(Constant.Blocks.get(i));
-                    if (itemMeta.getCustomModelData() == moddedBlock.getId() - 1) {
+                    System.out.println(Constant.Blocks.get(i).getId());
+                    if (itemMeta.getCustomModelData() - 1 == moddedBlock.getId()) {
                         return moddedBlock;
                     }
                 }
